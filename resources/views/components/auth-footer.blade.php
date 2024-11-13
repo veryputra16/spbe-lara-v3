8d69146394156a2ee4d40bc25212f74d4d8a6198
putuokky <okkymahes@gmail.com>
2024-11-13 15:07:34 +0800

    benerin tampilan login

--- resources/views/components/auth-footer.blade.php
+++ resources/views/components/auth-footer.blade.php
@@ -1,3 +1,3 @@
   <div class="simple-footer">
-      Copyright &copy; Stisla 2018
+      Copyright &copy; {{ env('APP_NAME') }} 2018 - {{ date('Y') }}
   </div>
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
     <div class="footer-left">
-        &copy; 2018 - {{ date('Y') }} &mdash; <b>{{ env('APP_NAME') }}</b> | Development by <a
-            href="https://spbe.denpasarkota.go.id/">Tim SPBE Kominfo Denpasar</a>
+        Copyright &copy; 2018 - {{ date('Y') }} &mdash; <b>{{ env('APP_NAME') }}</b>
+        <div class="bullet"></div> Development by <a href="https://spbe.denpasarkota.go.id/">Tim SPBE Kominfo Denpasar</a>
     </div>
     <div class="footer-right">
         v3.18.0125
