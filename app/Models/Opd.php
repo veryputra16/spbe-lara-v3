9b2c66076e7751cddc6c80ae0b25a437c36480e6
Bagustirta <80937265+Bagustirta@users.noreply.github.com>
2025-07-08 14:04:18 +0800

    dashboard

--- app/Models/Opd.php
+++ app/Models/Opd.php
@@ -19,4 +19,9 @@ class Opd extends Model
     {
         return $this->hasMany(Opd::class);
     }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
+    }
 }
