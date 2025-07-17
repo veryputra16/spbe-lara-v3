9309e9b9f3214c15fa750bd23d5c0c38045718c7
KapakTujeng <Gede2111.pramanda@gmail.com>
2025-07-17 15:25:33 +0800

    Display 'Pusat' and 'Lokal' labels inside badges in OPD table

--- resources/views/dashboard/aplikasi/d-app.blade.php
+++ resources/views/dashboard/aplikasi/d-app.blade.php
@@ -142,45 +142,47 @@
                                     <table class="table table-bordered table-hover" id="myTable">
                                         <thead>
                                             <tr>
-                                                <th>Nama Perangkat Daerah</th>
-                                                <th>Aplikasi</th>
-                                                <th>Pusat | Lokal</th>
-                                                <th>Status</th>
-                                                <th>Action</th>
+                                                <th class="text-center">Nama Perangkat Daerah</th>
+                                                <th class="text-center">Aplikasi</th>
+                                                <th class="text-center">Wilayah</th>
+                                                <th class="text-center">Status</th>
+                                                <th class="text-center">Action</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                                @foreach ($opds as $opd)
-                                                    <tr>
-                                                        <td>{{ $opd->nama }}</td>
-                                                        <td>{{ $opd->applications->count() }}</td>
-                                                        <td>
-                                                            <div class="badge badge-secondary text-dark">
-                                                                {{ $opd->pusat_count ?? 0 }}
-                                                                <i class="fas fa-building"></i>
-                                                            </div>
-                                                            <div class="badge badge-secondary text-dark">
-                                                                {{ $opd->lokal_count ?? 0 }}
-                                                                <i class="fas fa-home"></i>
-                                                            </div>
-                                                        </td>
-                                                        <td>
-                                                            <div class="badge badge-secondary text-dark">
-                                                                {{ $opd->aktif_count ?? 0 }}
-                                                                <i class="fas fa-check-circle text-success"></i>
-                                                            </div>
-                                                            <div class="badge badge-secondary text-dark">
-                                                                {{ $opd->nonaktif_count ?? 0 }}
-                                                                <i class="fas fa-times-circle text-danger"></i>
-                                                            </div>
-                                                        </td>
-                                                        <td>
-                                                            <a href="{{ route('admin.application.index', ['opd' => strtolower($opd->nama)]) }}" class="btn btn-secondary">
-                                                                <i class="fas fa-info-circle text-dark"></i>
-                                                            </a>
-                                                        </td>
-                                                    </tr>
-                                                @endforeach
+                                        @foreach ($opds as $opd)
+                                            <tr>
+                                                <td class="text-center">{{ $opd->nama }}</td>
+                                                <td class="text-center">{{ $opd->applications->count() }}</td>
+                                                <td class="text-center">
+                                                    <div class="justify-content-center gap-2">
+                                                        <div class="badge badge-secondary text-dark">
+                                                            Pusat : {{ $opd->pusat_count ?? 0 }}
+                                                            <i class="fas fa-building"></i>
+                                                        </div>
+                                                        <div class="badge badge-secondary text-dark">
+                                                            Lokal : {{ $opd->lokal_count ?? 0 }}
+                                                            <i class="fas fa-home"></i>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td class="text-center">
+                                                    <div class="badge badge-secondary text-dark">
+                                                        {{ $opd->aktif_count ?? 0 }}
+                                                        <i class="fas fa-check-circle text-success"></i>
+                                                    </div>
+                                                    <div class="badge badge-secondary text-dark">
+                                                        {{ $opd->nonaktif_count ?? 0 }}
+                                                        <i class="fas fa-times-circle text-danger"></i>
+                                                    </div>
+                                                </td>
+                                                <td class="d-flex justify-content-center">
+                                                    <a href="{{ route('admin.application.index', ['opd' => strtolower($opd->nama)]) }}" class="btn btn-secondary">
+                                                        <i class="fas fa-info-circle text-dark"></i>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
                                         </tbody>
                                     </table>
                                 </div>
